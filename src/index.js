44dc85559bde163151401cad2c4c5c4f44a9d481
Thiago Martins <thiago_valentim1@hotmail.com>
2026-03-08 10:17:12 -0300

    chore: cria exemplo de modelo

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import { databaseConfig } from './config/database.config.js';
 import { criaConexaoDB } from './db/client.factory.js';
+import Autor from './models/autor.js';
 
 using conexao = criaConexaoDB(databaseConfig);
-const autores = await conexao.select('*').from('autores');
+Autor.configurarDB(conexao);
+const autores = await Autor.pegarAutores();
 console.log(autores);
---
+++ src/models/autor.js
@@ -0,0 +1,65 @@
+class Autor {
+  static db;
+
+  static configurarDB(db) {
+    this.db = db;
+  }
+
+  constructor({ id, nome, nacionalidade, created_at, updated_at }) {
+    this.id = id;
+    this.nome = nome;
+    this.nacionalidade = nacionalidade;
+    this.created_at = created_at || new Date().toISOString();
+    this.updated_at = updated_at || new Date().toISOString();
+  }
+
+  static async pegarAutores() {
+    return this.db.select('*').from('autores');
+  }
+
+  static async pegarPeloId(id) {
+    const resultado = await this.db.select('*').from('autores').where({ id });
+    return resultado[0];
+  }
+
+  async criar() {
+    return this.db('autores')
+      .insert(this)
+      .then((registroCriado) =>
+        this.db('autores').where('id', registroCriado[0]),
+      )
+      .then((registroSelecionado) => new Autor(registroSelecionado[0]));
+  }
+
+  async atualizar(id) {
+    // o update retorna a quantidade de rows atualizados e não o objeto do registro atualizado
+    await this.db('autores')
+      .where({ id })
+      .update({ ...this, updated_at: new Date().toISOString() });
+
+    return this.db.select('*').from('autores').where({ id });
+  }
+
+  static async excluir(id) {
+    // o del retorna a quantidade de rows deletados
+    await this.db('autores').where({ id }).del();
+  }
+
+  async salvar() {
+    // verificar se o id existe no banco
+    // se não existir é create
+    // se existir é update
+    if (this.id) {
+      const resultado = await this.atualizar(this.id);
+      return resultado;
+    }
+    const resultado = await this.criar();
+    return resultado;
+  }
+
+  static async pegarLivrosPorAutor(autorId) {
+    return this.db('livros').where({ autor_id: autorId });
+  }
+}
+
+export default Autor;
